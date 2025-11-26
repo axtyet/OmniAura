1950b0d18f998381f4b0107d31c664b6b0c92d6f
GitHub Action <action@github.com>
2025-11-26 22:53:18 +0800

    2025-11-26 22:53

--- XPTV/js/bdys.js
+++ XPTV/js/bdys.js
@@ -2,15 +2,15 @@ const cheerio = createCheerio()
 const CryptoJS = createCryptoJS()
 const UA = 'Mozilla/5.0 (iPhone; CPU iPhone OS 18_2 like Mac OS X) AppleWebKit/604.1.14 (KHTML, like Gecko)'
 const headers = {
-  'Referer': 'https://v.xlys.ltd.ua/',
-  'Origin': 'https://v.xlys.ltd.ua',
+  'Referer': 'https://www.xlys02.com/',
+  'Origin': 'https://www.xlys02.com',
   'User-Agent': UA,
 }
 
 const appConfig = {
   ver: 1,
   title: "哔滴影视",
-  site: "https://v.xlys.ltd.ua",
+  site: "https://www.xlys02.com",
   tabs: [{
     name: '最新电影',
     ext: {
@@ -144,26 +144,31 @@ async function getPlayinfo(ext) {
     headers
   })).data
   
-  if (JSON.parse(data2).data.url3) {
-    let url3 = JSON.parse(data2).data.url3
-    let play = url3.indexOf(',') !== -1 ? url3.split(',')[0].trim() : url3.trim()
-    return jsonify({
-      urls: [play],
-    })
-  } else if (JSON.parse(data2).data.tos) {
+  if (JSON.parse(data2).data.tos) {
     let god = `${appConfig.site}/god/${pid}?type=1`
     let res = await $fetch.post(god, {
       t: currentTimeMillis,
       sg: encryptedString,
       verifyCode: 888,
     }, {
-      'User-Agent': headers,
-      'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+      headers:{
+        'User-Agent': UA,
+        'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+        'X-Requested-With': 'XMLHttpRequest'
+      }
     })
     
     let playUrl = argsify(res.data).url
     return jsonify({
       urls: [playUrl],
+      headers: [{'User-Agent': UA}]
+    })
+  } else if (JSON.parse(data2).data.url3) {
+    let url3 = JSON.parse(data2).data.url3
+    let play = url3.indexOf(',') !== -1 ? url3.split(',')[1].trim() : url3.trim()
+    return jsonify({
+      urls: [play],
+      headers: [headers]
     })
   } else {
     let god = `${appConfig.site}/god/${pid}`
@@ -179,6 +184,7 @@ async function getPlayinfo(ext) {
     let playUrl = argsify(res.data).url
     return jsonify({
       urls: [playUrl],
+      headers: [headers]
     })
   }
 }
